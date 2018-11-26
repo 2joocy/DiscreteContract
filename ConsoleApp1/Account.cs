0f695f26a289fa2b2abf5e963fe4084f6b288024
William Pfaffe <william.pfaffe@gmail.com>
2018-11-27 00:53:50 +0100

    Initial Commit

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+    class Account
+    {
+        private float balance;
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+        public void deposit(float deposit) {
+            Contract.Requires<Exception>(deposit < 0, "Deposit value cannot be negative");
+            balance += deposit;
+        }
+
+        public void withdraw(float deposit) {
+            Contract.Requires<Exception>(deposit < 0, "Deposit value cannot be negative");
+            Contract.Requires<Exception>(deposit > balance, "Cannot withdraw a larger amount than the balance");
+            balance -= deposit;
+            
+        }
+
+        public float getBalance() {
+            return this.balance;
+        }
+
+    }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static Account acc;
+
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            acc.setBalance(1000);
+            negativeDeposit();
+            negativeWithdraw();
+            largeWithdraw();
+        }
+
+        public static void negativeDeposit() {
+            acc.deposit(-100);
+        }
+
+        public static void negativeWithdraw() {
+            acc.withdraw(-100);
+        }
+
+        public static void largeWithdraw() {
+            acc.withdraw(2000);
+        }
+
+
+    }
+}
